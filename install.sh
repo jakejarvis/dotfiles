c9c49c2c59ac7f7c2cb2bde0744ba22fef8ec6bc
Jake Jarvis <jake@jarv.is>
2021-01-12 13:46:03 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ echo "👋  Deep breaths, everything will (probably) be fine!"
 ln -sf "$HOME"/.dotfiles/zsh/.zshrc "$HOME"/.zshrc
 ln -sf "$HOME"/.dotfiles/git/.gitconfig "$HOME"/.gitconfig
 ln -sf "$HOME"/.dotfiles/git/.gitignore_global "$HOME"/.gitignore_global
+mkdir -p "$HOME"/.ssh
 ln -sf "$HOME"/.dotfiles/ssh/.ssh/config "$HOME"/.ssh/config
 
 # Get Oh My ZSH up and running
